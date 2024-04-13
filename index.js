6ca383a766d7b9ca65cefffcf83e1ae0d184b237
Zhenghang Zhong <zhenghang.zhong@bahag.com>
2024-04-13 11:44:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core')
+const core = require('@actions/github')
+
+(
+  async() => {
+    try{
+      core.notice("Calling our action");
+    } catch (error){
+      core.setFailed(error.message);
+    }
+  }
+)();
